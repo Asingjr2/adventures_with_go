e6c2625db074f875b909e2d4d50073f839e941a2
asingjr2 <asingjr2@gmail.com>
2018-10-04 12:34:24 -0400

    updated cards projects with more functions.

--- wk_10-1/cards/deck.go
+++ wk_10-1/cards/deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // declaring new data type
 type deck []string
@@ -26,7 +32,44 @@ func newDeck() deck {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
-
 	return cards
+}
 
+func deal(d deck, handSize int) (deck, deck) {
+	// returns two deck objects of the divided original
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	// changes information to string using std package
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	// taking arguement to write new file
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	// using built in function from util to read file
+	// converting returned byte slice => string => deck
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// if no errors than move forward with code
+		// err code of 0 means nothing is wrong
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	// swaps index with random num between 0 and len(deck)
+	for i := range d {
+		newPos := rand.Intn(len(d) - 1)
+
+		// format does not exist in python...need temp value to swap
+		d[i], d[newPos] = d[newPos], d[i]
+	}
 }
--- wk_10-1/cards/main.go
+++ wk_10-1/cards/main.go
@@ -1,13 +1,17 @@
 // Works a bit like C# and other static langauges with type definition
 package main
 
-import "fmt"
-
 func main() {
+	// creating deck with for loops
 	cards := newDeck()
-	fmt.Println(cards)
+	cards.saveToFile("all_cards")
 
-	cards.print()
+	// creating deck object from saved file with separated values
+	cards2 := newDeckFromFile("all_cards")
+	cards2.print()
+
+	cards2.shuffle()
+	cards2.print()
 
 }
 
