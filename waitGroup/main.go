f42b3c5e8cbeef9cb5c42bdcc20540d08c181ab8
asingjr2 <asingjr2@gmail.com>
2018-10-22 18:26:21 -0400

    updated waitgroup

--- waitGroup/main.go
+++ waitGroup/main.go
@@ -1,5 +1,6 @@
 // Experimenting waitgroup which creates a default coutner that waits for a number
 // of activities to finish.  wg.Add(1) gives total of 1 activity to finish
+// If waitGroup not used in below code, Foo() will not ever complete since the program will close.
 package main
 
 import (
@@ -9,13 +10,13 @@ import (
 )
 
 const (
+	// Checking specific PC elements
 	arch = runtime.GOARCH
 	os   = runtime.GOOS
 )
 
 var wg sync.WaitGroup // creating package level waitgroup
 
-// Checking specific PC elements
 func main() {
 	fmt.Println("architecture", arch)
 	fmt.Println("os", os)
@@ -23,10 +24,8 @@ func main() {
 	fmt.Println("routines", runtime.NumGoroutine())
 
 	// If run outright completes because main func code finishes first
-	go play()
-
 	wg.Add(1)
-	foo()
+	go foo() // would not complete without telling program to wait
 	bar()
 
 	fmt.Println("cpus", runtime.NumCPU())
@@ -35,22 +34,16 @@ func main() {
 }
 
 // Go routine set to run below which cannot complete before main finishes
-// If max i dropped to 2 program could possible run before main finishes
-func play() {
-	for i := 0; i < 1000; i++ {
-		fmt.Println("play", i)
-	}
-}
 
 func foo() {
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println("foo", i)
-		wg.Done()
 	}
+	wg.Done()
 }
 
 func bar() {
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println("bar", i)
 	}
 }
