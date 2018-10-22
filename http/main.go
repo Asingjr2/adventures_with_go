4d68acd7682e2c1cec0a7b091f686edad90206f4
asingjr2 <asingjr2@gmail.com>
2018-10-22 11:28:44 -0400

    updated files

---
+++ http/main.go
@@ -0,0 +1,15 @@
+// Experimenting with http package
+package main
+
+import (
+	"net/http"
+)
+
+type samusClient struct {
+	client *http.Client
+}
+
+func main() {
+	client := samusClient{}
+
+}
--- udemyExcercises/introEx/main.go
+++ udemyExcercises/introEx/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+var a int
+var b string
+var c bool
+
+var d int = 42
+var e = "James Bond"
+
 func main() {
-	
+	// below prints "zero" values for type, 0, blank, and false
+	fmt.Print(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	// Formatted lines
+	sol := fmt.Sprintf("%v\t%v\t", d, e)
+	fmt.Print(sol)
 }
