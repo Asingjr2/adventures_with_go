e228e72e14f3986d934b8e7d73d1445e694dedd9
asingjr2 <asingjr2@gmail.com>
2018-10-26 15:36:17 -0400

    updated interface information.

--- interface/main.go
+++ interface/main.go
@@ -1,4 +1,5 @@
 // Interface determined by method set for type
+// Can replace identical named methods for shared use
 package main
 
 import "fmt"
@@ -7,17 +8,46 @@ type (
 	// Creating bare structs so we can just build methods for them
 	englishBot struct{}
 
-	spanishBot struct {}
+	spanishBot struct{}
+
+	// Creating method that will be shared by types with similiar functions
+	// Anything that has greeeting method is considered a bot
+	bot interface {
+		greeting() string
+	}
 )
 
 func main() {
-	
+	// Creating instances of bot that have no methods/ attributes
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
 }
 
-func (e englishBot) greeting() string {
+// Specific to englishBot
+func (eb englishBot) greeting() string {
 	return "Sup"
 }
 
-func (s spanishBot) greeting() string {
+// Specific to spanishBot
+func (sb spanishBot) greeting() string {
 	return "Hola"
+}
+
+// Function to print greeting that will repalced by interface
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.greeting())
+// }
+
+// Function to print greeting replaced by interface
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.greeting())
+// }
+
+// Function takes a type bot.  Because english and spanish spot are also type bot.  they can be used.
+func printGreeting(b bot) {
+	fmt.Println(b.greeting())
 }
