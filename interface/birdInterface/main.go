d607b75381f3e6feef094ca43d0807a221236918
asingjr2 <asingjr2@gmail.com>
2018-10-29 21:48:42 -0400

    updated bir interface example.

--- interface/birdInterface/main.go
+++ interface/birdInterface/main.go
@@ -9,44 +9,7 @@ type (
 	cardinal struct{}
 	blueJay  struct{}
 	bird     interface {
-		whatTypeAmI() 
-	}
-)
-
-func (c cardinal) whatTypeAmI() {
-	fmt.Println("I am a cardinal")
-}
-
-func (b blueJay) whatTypeAmI() {
-	fmt.Println("I am a bluejay")
-}
-
-func myTypeIs(niceBird bird) {
-	fmt.Println("I am a glorious BIRDDDD")
-}
-
-func main() {
-	// Creating
-	card := cardinal{}
-	blue := blueJay{}
-
-	card.whatTypeAmI()
-	blue.whatTypeAmI()
-
-	myTypeIs(card)
-	myTypeIs(blue)
-}// Creating bird interface in golang
-package main
-
-import (
-	"fmt"
-)
-
-type (
-	cardinal struct{}
-	blueJay  struct{}
-	bird     interface {
-		whatTypeAmI() 
+		whatTypeAmI()
 	}
 )
 
@@ -60,7 +23,7 @@ func (b blueJay) whatTypeAmI() {
 	fmt.Println("I am a bluejay")
 }
 
-// Creating a function that takes a type interface.  
+// Creating a function that takes a type interface.
 func myTypeIs(niceBird bird) {
 	fmt.Println("I am a glorious BIRDDDD")
 }
